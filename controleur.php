0bd04b41c5eb5da7c33b2cdaaec524e5e6108001
Ferrer Jeremy <ferrer.jeremy3@gmail.com>
2016-09-21 19:36:44 +0200

    marche en 2x2

--- controleur.php
+++ controleur.php
@@ -1,66 +1,81 @@
 <?php
 
-set_time_limit (10); //Execution time MAX in seconds
+set_time_limit (6); //Execution time MAX in seconds
 
 if(isset($_POST['textArea']))
 {
 	//Input comming from the view
 	$input = $_POST['textArea'];
 
-	$arrayLigne = array(1,2,3,4,5,6,7,8,9);
+	
+	$arrayNumbers = array(1,2,3);//1,2,3,4,5,6,7,8,9);// array de reference
+	$arrayForRand = $arrayNumbers;//array sur lequel on fait un rand
 
-	$arrayComplet = array();
+	$arrayLigne = array();//array d'une ligne a ajouter une fois fini a arraycomplet
+	$arrayComplet = array();//array final
 
-	shuffle($arrayLigne);//suffle la premiere ligne
-	array_push($arrayComplet, $arrayLigne);//ajoute la premiere ligne
-	$tours = 0;
-
-	for ($i=1; $i < 7; $i++) //pour faire 6 lignes + la premiere qui est deja faite
-	{ 
-		$checkLigne = true;
-		do
-		{
-			$errorLigne = 0;
-			shuffle($arrayLigne);
-			foreach ($arrayComplet as $arrayLigneInside) 
+	for ($i=0; $i < 100; $i++) { 
+		
+		for ($ligne = 0; $ligne < 3; $ligne++) //pour chaque ligne
+		{ 
+			for ($case = 0; $case < 3; $case++) //pour chaque case d'une ligne
 			{
-				for ($y=0; $y < 9; $y++) 
+				$arrayForRand = $arrayNumbers;
+
+				for ($preligne=0; $preligne < $ligne; $preligne++)  //on enleve les nombres deja sorti sur cette colone
 				{ 
-					if($arrayLigneInside[$y] == $arrayLigne[$y])
-					{
-						$errorLigne++;
-						break;
-					}
+					$key = $arrayComplet[$preligne][$case];
+					$key--;//l'array commence a 0
+					$arrayForRand[$key] = null;
 				}
 
-				if ($errorLigne != 0) 
+				for ($precase=0; $precase < $case; $precase++) //on enleve les nombres deja sorti sur cette ligne
+				{ 
+					$key = $arrayLigne[$precase]; 
+					$key--;//l'array commence a 0
+					$arrayForRand[$key] = null;
+				}
+
+				do //on cherche parmi les nombres restant
 				{
-					break;
+					$randNumber = array_rand($arrayForRand);
 				}
+				while($arrayForRand[$randNumber] == null);
+				
+				array_push($arrayLigne, $arrayForRand[$randNumber]);
 			}
-
-			if ($errorLigne == 0) //si il n'y a pas d'erreur on sort de la boucle
-			{
-				$checkLigne = false;
-			}
-			$tours ++;
+			array_push($arrayComplet, $arrayLigne);//ajoute la  ligne
+			$arrayLigne = array();//on reset l'array ligne
 		}
-		while($checkLigne);
-
-		array_push($arrayComplet, $arrayLigne); //ajoute la ligne suivante validé
+		$taille = sizeof($arrayComplet)-1;
+		$output = json_encode($arrayComplet);
+		$arrayComplet = array();
+		echo "<br>";
+		echo $output;
 	}
 
-	//je deduis les 2 lignes restantes
 
 
 
 
+
+
+
+
+
+
+	
+
+
+	
+
+
+	//var_dump($arrayComplet);
 
 	//Output to be send to the view
-	array_push($arrayComplet, array($tours));
 
 	$output = json_encode($arrayComplet);
-	echo $output;
+	//echo $output;
 }
 else
 {
